e1320dbe1fdd02ed40fea7ca2fb1d382a8e622e1
AnthonyTerreros <dterrero@espol.edu.ec>
2022-08-12 19:48:41 -0500

    Creacion de Ruta Productos

---
+++ proyecto3/myshopappserver/controllers/producto.controller.js
@@ -0,0 +1,29 @@
+
+
+const obtenerProductos = (req, res, next) => {
+
+}
+
+const obtenerProductoPorId = (req, res, next) => {
+
+}
+
+const crearProducto = (req, res, next) => {
+
+}
+
+const actualizarProducto = (req, res, next) => {
+
+}
+
+const eliminarProducto = (req, res, next) => {
+
+}
+
+module.exports = {
+    obtenerProductos,
+    obtenerProductoPorId,
+    crearProducto,
+    actualizarProducto,
+    eliminarProducto
+}
---
+++ proyecto3/myshopappserver/routes/producto.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const { obtenerProductos, obtenerProductoPorId, crearProducto, actualizarProducto, eliminarProducto } = require('../controllers/producto.controller')
+
+const router = express.Router()
+
+
+router.get("/productos", obtenerProductos)
+
+router.get("/productos/:id", obtenerProductoPorId)
+
+router.post("/productos", crearProducto)
+
+router.put("/productos/:id", actualizarProducto)
+
+router.delete("/productos/:id", eliminarProducto)
+
+
+module.exports = router
