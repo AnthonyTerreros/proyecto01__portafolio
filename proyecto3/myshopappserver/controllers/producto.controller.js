057482ef6ab2897193c935580c0a5e14a168ebc9
Anthony Terreros <dterrero@espol.edu.ec>
2022-08-13 23:20:22 -0500

    Rutas Productos y Categorias Terminadas

--- proyecto3/myshopappserver/controllers/producto.controller.js
+++ proyecto3/myshopappserver/controllers/producto.controller.js
@@ -13,23 +13,64 @@ const obtenerProductos = (req, res, next) => {
 };
 
 const obtenerProductoPorId = (req, res, next) => {
-  Producto.findOne({ where: { id: req.params.id } })
+  Producto.findOne({attributes: {exclude: ['categoriumId']}, where: { id: req.params.id } })
     .then((photos) => {
       res.json(photos);
     })
     .catch((error) => res.send(error));
 };
 
-const crearProducto = (req, res, next) => {
-  res.send("Post Works!");
+const crearProducto = async (req, res, next) => {
+  const { name, stock, url_imagen, precio, categoriaId} = req.body;
+  try {
+    await Producto.create({
+      name,
+      stock,
+      url_imagen,
+      precio,
+      categoriaId
+    },
+    {
+      fields: ["name", "stock", "url_imagen", "precio", "categoriaId"],
+    });
+    res.json({ message: "Producto Creado!" });
+  } catch (error) {
+    res.status(500).json({ message: "No se pudo crear el producto!" });
+  }
 };
 
-const actualizarProducto = (req, res, next) => {
-  res.send("Put Works!");
+const actualizarProducto = async (req, res, next) => {
+  try {
+    let id_pk = req.params.id;
+    const { name, stock, url_imagen, precio, categoriaId} = req.body;
+    let producto = await Producto.findByPk(id_pk);
+    producto.name = name;
+    producto.stock = stock;
+    producto.url_imagen = url_imagen;
+    producto.precio = precio;
+    producto.categoriaId = categoriaId;
+    await producto.save();
+    res.json({ message: "Producto Actualizado!" });
+  } catch (error) {
+    console.log(error)  
+    res.status(500).json({ message: "No se pudo actualizar el producto. Intenta mas tarde!" });
+  }
 };
 
-const eliminarProducto = (req, res, next) => {
-  res.send("Delete Works!");
+const eliminarProducto = async (req, res, next) => {
+  try {
+    await Producto.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({ message: "Producto Eliminada!" });
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ message: "El Producto no pudo ser eliminada!" });
+  }
 };
 
 module.exports = {
